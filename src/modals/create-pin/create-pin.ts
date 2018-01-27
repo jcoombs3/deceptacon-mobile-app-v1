ae6caf6389d49ad1f708ab3ffece2bf26567dd87
John Coombs <johnc@backbase.com>
2018-01-26 23:50:28 -0500

    pin-pad confirmation

--- src/modals/create-pin/create-pin.ts
+++ src/modals/create-pin/create-pin.ts
@@ -6,6 +6,9 @@ import { ViewController, ToastController } from 'ionic-angular';
   templateUrl: 'create-pin.html'
 })
 export class CreatePinModal {
+  counter: number = 0;
+  pin: any = [];
+  confirmPIN: boolean = false;
   
   constructor(
     public viewCtrl: ViewController,
@@ -15,13 +18,42 @@ export class CreatePinModal {
   }
   
   completePIN(pin: any) {
-    let toast = this.toastCtrl.create({
-      message: 'Account PIN created',
-      duration: 2000,
-      position: 'top',
-      showCloseButton: true
-    });
-    toast.present();
-    this.viewCtrl.dismiss(pin);
+    if (this.counter === 0) {
+      this.counter++;
+      this.pin = pin;
+      this.confirmPIN = true;
+    } else {
+      if (this.validatePIN(pin)) {
+        let toast = this.toastCtrl.create({
+          message: 'Account PIN created',
+          duration: 2000,
+          position: 'top',
+          showCloseButton: true
+        });
+        toast.present();
+        this.viewCtrl.dismiss(pin);
+      } else {
+        let toast = this.toastCtrl.create({
+          message: 'Your PIN was not the same. Try again',
+          duration: 2000,
+          position: 'top',
+          showCloseButton: true,
+          cssClass: 'error'
+        });
+        toast.present();
+        this.counter = 0;
+        this.pin = [];
+        this.confirmPIN = false;
+      }   
+    }
+  }
+  
+  validatePIN(pin: any) {
+    for (let i = 0; i < pin.length; i++) {
+      if (pin[i] !== this.pin[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
